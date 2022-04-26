9f4f0ae0b9f242af56d7f60eec0451f9c257175b
Munkh-Erdene <munkherdene.ts9234@gmail.com>
2022-04-26 17:58:58 +0800

    codecov removed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Link, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import Home from './Home';
 import Aboutus from './Aboutus';
